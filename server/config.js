2fed6b151f834b66c0b746d7b6fc44fb38ad6f4a
Javier Elizalde <javelisol@gmail.com>
2018-04-15 15:03:37 -0500

    Set up the server files

---
+++ server/config.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const bodyParser + require("body-parser");
+const route = require("./routes");
+const app = express();
+
+//middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+app.use(express.static(__dirname + "/../client/dist"));
+
+module.exports = app;
